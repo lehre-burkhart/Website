8baa5c253c25200eb86e58233d480a2a9d8e3247
Christian Burkhart <burkhart.christian@gmail.com>
2017-02-16 09:42:16 +0100

    feat: enable interactive worked example in second task (aufgabe2)
    Durch einen Buttonklick wird die Musterlösung angezeigt. Die
    Funktionalität wird durch Jquery's Toggle Funktion ermöglicht. Das
    Boostrap Grid system Merkblatt und Musterbeispiel in je 50% der
    Bildbreite.

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function() {
-    console.log("ready!");
+    ///////////////////////////////////////////////
+    // Feedback nehmen
+    ///////////////////////////////////////////////
+
+    // Toggle Feedback nehmen solution when
+    // button is clicked
+
+    // https://www.w3schools.com/jquery/jquery_ref_selectors.asp
+    $('#fn-button').click(function() {
+      // http://api.jquery.com/toggle/
+      $('#fn-aufgabe2-worked-example').toggle();
+    })
 });
